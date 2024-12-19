f23a12b5a8da1a3c922667bd1a6f98d306057318
veravanhoogdalem <veravanh@outlook.com>
2024-12-19 14:54:33 +0100

    Integrated API, added search city function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+   let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+let apiKey = "84b4398430f91bcff96523d5at640feo";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event) {
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+
+
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
